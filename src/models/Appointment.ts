6ef053d3a3b448d7b2bdd3fd0086ceefddb607d3
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2020-10-19 20:30:33 -0300

    Appointments repository, model, service and database connection

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,22 +1,15 @@
-import { v4 as uuid_v4 } from 'uuid';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-// interface AppointmentConstructor {
-//   provider: string;
-//   date: Date;
-// }
-
+@Entity('appointments') // Table name
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('timestamp with time zone')
   date: Date;
-
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid_v4();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,28 +1,28 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentsServices from '../services/CreateAppointmentsServices';
 
 const appoitmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
+// const appointmentsRepository = new AppointmentsRepository();
 
-appoitmentsRouter.get('/', (req, res) => {
-  const appointments = appointmentsRepository.all();
+appoitmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return res.json(appointments);
 });
 
-appoitmentsRouter.post('/', (req, res) => {
+appoitmentsRouter.post('/', async (req, res) => {
   try {
     const { provider, date } = req.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentsServices(
-      appointmentsRepository
-    );
+    const createAppointment = new CreateAppointmentsServices();
 
-    const appointment = createAppointment.execute({
+    const appointment = await createAppointment.execute({
       date: parsedDate,
       provider,
     });
